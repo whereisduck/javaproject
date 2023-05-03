97fa7b17a3718896f6f242ba14b3a50bb0ae7d3b
kenzh <zyq1984ny@gmail.com>
2023-05-03 12:11:37 -0400

    added comment of benifit of interface

--- src/privateStaticMethodOfInterface/PrivateMethodDoor.java
+++ src/privateStaticMethodOfInterface/PrivateMethodDoor.java
@@ -1,5 +1,11 @@
 package privateStaticMethodOfInterface;
 //form java 9 can user private methods in interface.
+/*Advantages of interfaces
+//Interfaces allow us to achieve 100% abstraction.
+//Interfaces can be used to achieve loose coupling in an application.
+This means that a change in one class doesn’t affect the implementation of the other class.
+//By the use of interfaces, one can break up complex designs and clear the dependencies between objects.
+Interfaces can be used to achieve multiple inheritance.*/
 public interface PrivateMethodDoor {
     default void foo(){
         System.out.println("default foo");
